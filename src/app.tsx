0d4934c55ce6a67602615d233a0e63d9a6e12a30
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-10-25 11:21:32 -0300

    refactor: switch from miragejs to json-server

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react';
+
+type User = {
+	login: string;
+	id: number;
+	avatar_url: string;
+	html_url: string;
+	name: string;
+	bio: string;
+	public_repos: number;
+	public_gists: number;
+	followers: number;
+	following: number;
+};
+
 export function App() {
+	const [user, setUser] = useState<User>({} as User);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch('http://localhost:3000/profile');
+			const data = await response.json();
+
+			setUser(data);
+		})();
+	}, []);
+
 	return (
 		<div className="flex items-center justify-center min-h-screen bg-zinc-950 text-zinc-50">
-			<h1>Hello, World!</h1>
+			{user ? (
+				<>
+					<h1>
+						Hello,{' '}
+						<a href={user.html_url} target="_blank" rel="noopener noreferrer">
+							{user.name}
+						</a>
+						!
+					</h1>
+					<p>{user.bio}</p>
+				</>
+			) : (
+				<h1>Hello, World!</h1>
+			)}
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,8 @@
-import { createServer } from 'miragejs';
 import { createRoot } from 'react-dom/client';
 import { App } from './app.tsx';
 
 import './index.css';
 
 const container = document.getElementById('root');
-
-createServer({
-	routes() {
-		this.namespace = 'api';
-		this.timing = 2000; // 2000ms (2 seconds)
-
-		this.get('/users/victorbadaro', () => ({
-			login: 'victorbadaro',
-			id: 9096344,
-			avatar_url: 'https://avatars.githubusercontent.com/u/9096344?v=4',
-			html_url: 'https://github.com/victorbadaro',
-			name: 'Victor Badaró',
-			bio: 'Full Stack developer (Frontend focused) and new technologies lover. The following technologies are part of my day to day usage (in different levels): Node JS, J',
-			public_repos: 155,
-			public_gists: 2,
-			followers: 133,
-			following: 265
-		}));
-	}
-});
 
 createRoot(container!).render(<App />);
